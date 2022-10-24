7c6f2332f6876d00c021e82df6eb2724af93ab18
PoH98 <shunyuan98@gmail.com>
2022-10-24 12:07:17 +0800

    added signalR in adventure game

--- pages/advgame/_advgame.vue
+++ pages/advgame/_advgame.vue
@@ -1,13 +1,29 @@
 <template>
-    <v-container>
-
-    </v-container>
+  <v-container> </v-container>
 </template>
 <script>
 export default {
-    mounted(){
-        //get room id
-        
-    }
-}
+  data() {
+    return {
+      connection: null,
+    };
+  },
+  mounted() {
+    //get room id
+    let id = this.$route.params.advgame;
+    const signalR = require("@microsoft/signalr");
+    console.log(signalR);
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl("/advhub")
+      .build();
+
+    this.connection.on("Connected", (data) => {
+      console.log(data);
+    });
+
+    this.connection
+      .start()
+      .then(() => this.connection.invoke("Connect"));
+  },
+};
 </script>
