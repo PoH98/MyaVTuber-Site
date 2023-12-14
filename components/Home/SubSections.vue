14b433c265479b95d60878bedf82875089a900da
PoH98 <shunyuan98@gmail.com>
2023-12-14 23:10:03 +0800

    updated

--- components/Home/SubSections.vue
+++ components/Home/SubSections.vue
@@ -151,16 +151,17 @@ export default {
   },
   methods: {
     getContrastYIQ(hexcolor) {
-      if (hexcolor && !this.theme.themes._value.dark.dark) {
+      if (hexcolor && !this.theme.global.current.value.dark) {
         hexcolor = hexcolor.replace("#", "");
         var r = parseInt(hexcolor.substr(0, 2), 16);
         var g = parseInt(hexcolor.substr(2, 2), 16);
         var b = parseInt(hexcolor.substr(4, 2), 16);
         var yiq = (r * 299 + g * 587 + b * 114) / 1000;
         return yiq >= 128 ? "text-black" : "text-white";
-      } else if (!this.theme.themes._value.dark.dark) {
+      } else if (!this.theme.global.current.value.dark) {
         return "text-black";
       } else {
+        console.log("Test")
         return "text-white";
       }
     },
